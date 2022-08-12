0f1598a55a2e1c37aafd072f8b4f7644ece44272
Daniel Garcia <danielagarciac02@gmail.com>
2022-08-12 14:42:19 -0500

    add host

--- casa_blanca/settings.py
+++ casa_blanca/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(5qoh5sru#9r@nz)52(2ya)g$6to+i1lwodh50wf+-2#)#p9$7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'comercialcasablanca.herokuapp.com']
 
 
 # Application definition
