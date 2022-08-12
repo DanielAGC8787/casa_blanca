1af1eeb352e45d3c48358dee24391c573b7cc8cb
Daniel Garcia <danielagarciac02@gmail.com>
2022-08-12 14:38:24 -0500

    make ready for web hosting

--- casa_blanca/settings.py
+++ casa_blanca/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(5qoh5sru#9r@nz)52(2ya)g$6to+i1lwodh50wf+-2#)#p9$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ eb-vert/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\danie\OneDrive\Development Programs\Django\casa_blanca\eb-vert\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
