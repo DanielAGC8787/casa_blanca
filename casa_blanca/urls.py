660cb8d76dbe32447bc6af8e38b766133ac81bd3
Daniel Garcia <danielagarciac02@gmail.com>
2022-08-13 06:33:26 -0500

    update static again

--- casa_blanca/urls.py
+++ casa_blanca/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main_site.urls")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
