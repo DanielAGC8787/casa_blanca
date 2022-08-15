8d923cdc86012e86dff8b0a51f5bbbb65df6d95d
Daniel Garcia <danielagarciac02@gmail.com>
2022-08-15 11:55:19 -0500

    fix import url error and remove socials

--- casa_blanca/urls.py
+++ casa_blanca/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.static import serve
-from django.conf.urls import url  
+# from django.conf.urls import path  
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main_site.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
