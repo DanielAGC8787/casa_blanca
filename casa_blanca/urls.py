2464ac7ba2a85d7857bef16659b0685d07cfac7e
Daniel Garcia <danielagarciac02@gmail.com>
2022-08-16 18:14:40 -0500

    fix issue of images only loading when debug = true

--- casa_blanca/urls.py
+++ casa_blanca/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
